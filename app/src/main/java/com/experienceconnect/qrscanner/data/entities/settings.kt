518df65fb284f2be6c5292fa346b6dc17d1e406b
hexch <hexch.c@gmail.com>
2019-06-14 19:59:44 +0900

    a@

---
+++ app/src/main/java/com/experienceconnect/qrscanner/data/entities/settings.kt
@@ -0,0 +1,4 @@
+package com.experienceconnect.qrscanner.data.entities
+
+@Entity
+data class Settings(var interval:Int = 1 ,var server:String)
--- app/src/main/java/com/experienceconnect/qrscanner/data/settings.kt
+++
@@ -1,3 +0,0 @@
-package com.experienceconnect.qrscanner.data
-
-data class Settings(var interval:Int = 1 ,var server:String)
